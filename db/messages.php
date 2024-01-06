76f01bb2588185900d12597c855ff6e580693cb9
Vernon Spain <verzog@gmail.com>
2024-01-07 06:08:42 +1000

    Update messages.php
    chat gpt 3.5

--- db/messages.php
+++ db/messages.php
@@ -22,8 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$messageproviders = array(
-
-	'expiry_notification' => array(),
-
-);
+$messageproviders = [
+    'expiry_notification' => [],
+];
